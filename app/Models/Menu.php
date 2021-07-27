89b3cf841d1ebe2e4c75396527bbb418a24b078a
Roi <intomarty@gmail.com>
2021-07-28 01:16:39 +0800

    search final

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -15,7 +15,7 @@ class Menu extends Model
     {
         if ($filters['search'] ?? false) {
             $query
-            ->where('name', 'like', '%' . $filters['search'] . '%');
+            ->where('name', 'ilike', '%' . $filters['search'] . '%');
         }
     }
     
